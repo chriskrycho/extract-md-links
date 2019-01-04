26bc2c9b7d1c86e0c75ff2e897fa890ca80b453c
Chris Krycho <hello@chriskrycho.com>
2019-01-03 22:14:45 -0700

    Add some more/better error-handling.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> Result<(), Box<std::error::Error + 'static>> {
     let mut args = std::env::args();
     let path = args.nth(1).expect("No argument supplied!");
     let contents = std::fs::read_to_string(&path)?;
-    Parser::new(&contents)
+    let links = Parser::new(&contents)
         .fold(
             (None, std::collections::HashMap::<String, Link>::new()),
             |(current, mut ls), event| match event {
@@ -75,19 +75,38 @@ fn main() -> Result<(), Box<std::error::Error + 'static>> {
                 _ => (None, ls),
             },
         )
-        .1
-        .iter()
-        .for_each(|(url, link)| match link {
-            Link::Complete {
-                text,
-                title: Some(title_text),
-            } => println!(r#"[{}]: {} "{}""#, text, url, title_text),
-            Link::Complete { text, title: None } => println!("[{}]: {}", text, url),
+        .1;
+
+    print!("\n");
+
+    links.iter().for_each(|(_, link)| match link {
+        Link::Complete { text, .. } => println!("- {}", text),
+        _ => {}
+    });
+
+    print!("\n");
+    links.iter().for_each(|(url, link)| match link {
+        Link::Complete {
+            text,
+            title: Some(title_text),
+        } => println!(r#"[{}]: {} "{}""#, text, url, title_text),
+        Link::Complete { text, title: None } => println!("[{}]: {}", text, url),
+        _ => {}
+    });
+
+    if links.iter().any(|(_, link)| match link {
+        Link::Partial { .. } => true,
+        _ => false,
+    }) {
+        eprint!("\nErrors:");
+        links.iter().for_each(|(url, link)| match link {
             Link::Partial {
                 title: Some(title_text),
             } => eprintln!(r#"BAD LINK: {} "{}""#, url, title_text),
             Link::Partial { title: None } => eprintln!("BAD LINK: {}", url),
+            _ => {}
         });
+    }
 
     Ok(())
 }
